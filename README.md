a2c8a2a57833568b9e3558ab9dee183373cd030e
esimpson-lilly <esimpson@lilly.com>
2025-02-25 11:21:18 -0500

    new plot

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # COVID_demo_demo
+
+
+
+- References:
+How to Map:
+https://medium.com/@mm.fuenteslopez/using-plotly-express-to-make-zip-code-level-choropleth-maps-a8ac8212b7ed
+GeoJSON Coordinate Repo
+https://github.com/OpenDataDE/State-zip-code-GeoJSON
--- example_from_stack_of.py
+++ example_from_stack_of.py
@@ -1,29 +1,19 @@
-import pandas as pd
-import pandas_bokeh
-import matplotlib.pyplot as plt
-import pgeocode
-import geopandas as gpd
-from shapely.geometry import Point
-from geopandas import GeoDataFrame
-pandas_bokeh.output_notebook()
-import plotly.graph_objects as go
+import plotly.express as px
+from urllib.request import urlopen
+import json
 
-nomi = pgeocode.Nominatim('us')
 
-edf = pd.read_csv('myFile.tsv', sep='\t',header=None, index_col=False ,names=['colC','zipcode','count'])
-edf['Latitude'] = (nomi.query_postal_code(edf['zipcode'].tolist()).latitude)
-edf['Longitude'] = (nomi.query_postal_code(edf['zipcode'].tolist()).longitude)
 
-fig = go.Figure(data=go.Scattergeo(
-        lon = edf['Longitude'],
-        lat = edf['Latitude'],
-        text = edf['colC'],
-        mode = 'markers',
-        marker_color = edf['count'],
-        ))
-
-fig.update_layout(
-        title = 'colC Distribution',
-        geo_scope='usa',
-    )
-fig.show()
+response =  open('in_indiana_zip_codes_geo.min.json', 'r')
+zipcodes = json.load(response)
+fig = px.choropleth(df, 
+                    geojson=zipcodes, 
+                    locations='ZIP_Code', 
+                    color='Cluster',
+                    color_continuous_scale="Viridis",
+                    range_color=(1,5),
+                    featureidkey="properties.ZCTA5CE10",
+                    scope="usa",
+                    labels={'Cluster':'Cluster_Category'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
